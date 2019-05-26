7e6d2b0d1b0a83d33a19a429d0cb6137cb4af643
joyce <jycyeo@yahoo.com.sg>
2019-05-26 22:36:04 +0800

    Add problem 10

---
+++ 010x.py
@@ -0,0 +1,17 @@
+# Problem 10
+
+LIMIT = 2000000
+is_not_prime = [0] * (LIMIT + 1)
+
+is_not_prime[0] = 1
+is_not_prime[1] = 1
+
+result = 2
+
+for i in range(3, LIMIT + 1, 2):
+    if is_not_prime[i] == 0:
+        result += i
+        for j in range(int(LIMIT + 1)/i):
+            is_not_prime[i * j] = 1
+
+print(result)
