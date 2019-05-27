1d8de5ef6de224b2b8c36e2d9b3eb0c53474fe1f
joyce <jycyeo@yahoo.com.sg>
2019-05-27 09:24:19 -0700

    Fix bug in int casting

--- 010x.py
+++ 010x.py
@@ -11,7 +11,7 @@ result = 2
 for i in range(3, LIMIT + 1, 2):
     if is_not_prime[i] == 0:
         result += i
-        for j in range(int(LIMIT + 1)/i):
+        for j in range(int((LIMIT + 1)/i)):
             is_not_prime[i * j] = 1
 
 print(result)
