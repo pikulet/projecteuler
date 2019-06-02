f1ef61289d310201aec0185292dd50a4bad4f3ed
joyce <jycyeo@yahoo.com.sg>
2019-06-02 20:14:58 +0800

    Add problem 33

---
+++ 033.py
@@ -0,0 +1,18 @@
+# Problem 33
+
+import itertools
+
+curious = []
+
+def is_curious:
+for i in range(10, 100):
+    for j in range(10, 100):
+
+    itertools.combinations_with_replacement(range(1, 10), 2):
+    for j in itertools.combinations_with_replacement(range(1, 10), 2):
+        if i == j:
+            pass
+        else:
+            if is_curious(i, j):
+                curious.append([i, j])
+
